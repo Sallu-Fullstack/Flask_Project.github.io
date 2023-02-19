21071b9a5250cf2a61b5dfa9eaceefcadd114cf6
Mohammed Salman <salludigitalm786@gmail.com>
2023-02-19 15:03:01 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 import pymysql
 
 con=None
 cur=None
 
 app=Flask(__name__)
+app.secret_key = "key_secret"
 
 def connectToDb():
     global con, cur
-    con=pymysql.connect(host="localhost", user="root", passwd="", database="pydbcon")
+    con=pymysql.connect(host="localhost", user="root", passwd="", database="pydbcon", port=3307)
     cur=con.cursor()
 def disconnectDB():
     cur.close()
     con.close()
 def getAllPersonData():
     connectToDb()
-    selectquery="SELECT * FROM person;"
+    selectquery="SELECT * FROM student;"
     cur.execute(selectquery)
     data=cur.fetchall()
     disconnectDB()
@@ -23,7 +24,7 @@ def getAllPersonData():
 def insertToPersonTable(name, mobile, email, city, dob=None):
     try:
         connectToDb()
-        insertQuery = "INSERT INTO person (name, mobile, email, city, DOB) VALUES (%s, %s, %s, %s, %s);"
+        insertQuery = "INSERT INTO student (name, mobile, email, city, DOB) VALUES (%s, %s, %s, %s, %s);"
         cur.execute(insertQuery, (name, mobile, email, city, dob))
         con.commit()
         disconnectDB()
@@ -31,29 +32,29 @@ def insertToPersonTable(name, mobile, email, city, dob=None):
     except:
         disconnectDB()
         return False
-def getOnePerson(person_id):
+def getOnePerson(stu_id):
     connectToDb()
-    selectquery = "SELECT * FROM person WHERE person_id=%s;"
-    cur.execute(selectquery, (person_id, ))
+    selectquery = "SELECT * FROM student WHERE stu_id=%s;"
+    cur.execute(selectquery, (stu_id, ))
     data = cur.fetchone()
     disconnectDB()
     return data
-def updatePersonToTable(name, mobile, email, city, person_id, dob=None):
+def updatePersonToTable(name, mobile, email, city, stu_id, dob=None):
     try:
         connectToDb()
-        updateQuery = "UPDATE person SET name=%s, mobile=%s, email=%s, city=%s,DOB=%s WHERE person_id=%s;"
-        cur.execute(updateQuery, (name, mobile, email, city, dob, person_id))
+        updateQuery = "UPDATE student SET name=%s, mobile=%s, email=%s, city=%s,DOB=%s WHERE stu_id=%s;"
+        cur.execute(updateQuery, (name, mobile, email, city, dob, stu_id))
         con.commit()
         disconnectDB()
         return True
     except:
         disconnectDB()
         return False
-def deletePersonFromTable(person_id):
+def deletePersonFromTable(stu_id):
     try:
         connectToDb()
-        deleteQuery = "DELETE FROM person WHERE person_id=%s;"
-        cur.execute(deleteQuery, (person_id))
+        deleteQuery = "DELETE FROM student WHERE stu_id=%s;"
+        cur.execute(deleteQuery, (stu_id))
         con.commit()
         disconnectDB()
         return True
@@ -67,6 +68,7 @@ def index():
     #return "<h1>Flask Programming!<h1>"
     #data is tuple of tuples
     data=getAllPersonData()
+    flash('This Project is Not Mobile Responsive! Please Avoid viewing in Mobile')
     return render_template("index.html", data=data)
 @app.route("/add/", methods=['GET', 'POST'])
 def addPerson():
@@ -81,12 +83,12 @@ def addPerson():
 
 @app.route("/edit/", methods=['GET', 'POST'])
 def updatePerson():
-    person_id = request.args.get('id', type=int, default=1)
-    data = getOnePerson(person_id)
+    stu_id = request.args.get('id', type=int, default=1)
+    data = getOnePerson(stu_id)
     if request.method == "POST":
         fdata = request.form
         # print(fdata)
-        if updatePersonToTable(fdata['txtName'], fdata['txtMobile'], fdata['txtEmail'], fdata['txtCity'], person_id, fdata['txtDOB']):
+        if updatePersonToTable(fdata['txtName'], fdata['txtMobile'], fdata['txtEmail'], fdata['txtCity'], stu_id, fdata['txtDOB']):
             message = "Record updated Successfully."
         else:
             message = "Due to some issues couldn't update record"
@@ -96,8 +98,8 @@ def updatePerson():
 @app.route("/delete/")
 def deletePerson():
     pass
-    person_id = request.args.get('id', type=int, default=1)
-    deletePersonFromTable(person_id)
+    stu_id = request.args.get('id', type=int, default=1)
+    deletePersonFromTable(stu_id)
     return redirect(url_for("index"))
 if __name__=='__main__':
-    app.run(debug=False,host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0')
